2fdb1d0dbb96b95ebf66d805f1853c4cd72f5c6a
Sérgio Avilla <sergiolucasavilladasilva@gmail.com>
2023-03-17 15:14:34 -0300

    add link branch to branch, add list options method

--- src/core/State.ts
+++ src/core/State.ts
@@ -1,9 +1,23 @@
+import Branch from "./Branch";
 import Message from "./Message";
 
 export default class State {
+
+    branchs: Branch[];
+
     constructor(
         readonly id: string,
         readonly message: Message,
         readonly catchMessage: Message
-    ) { }
+    ) {
+        this.branchs = [];
+    }
+
+    link(branch: Branch): void {
+        this.branchs.push(branch);
+    }
+
+    options(): string[] {
+        return this.branchs.map(branch => branch.label);
+    }
 }
