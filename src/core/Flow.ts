6f15eb408f4603a124d1f48612c8aebf29fad40b
Sérgio Avilla <sergiolucasavilladasilva@gmail.com>
2023-03-17 15:15:34 -0300

    fix flow class

--- src/core/Flow.ts
+++ src/core/Flow.ts
@@ -1,22 +1,15 @@
-import Branch from "./Branch";
 import State from "./State";
 
 export default class Flow {
-    branchs: Branch[];
     states: State[];
 
-    constructor() {
-        this.branchs = [];
+    constructor(
+        options: FlowOptions
+    ) {
         this.states = [];
     }
+}
 
-    addState(state: State) {
-        this.states.push(state);
-    }
-
-    addBridge(branch: Branch) {
-        if (!this.states.includes(branch.from)) throw "From state not found!";
-        if (!this.states.includes(branch.to)) throw "To state not found!";
-        this.branchs.push(branch);
-    }
+export interface FlowOptions {
+    defaultState: State
 }
