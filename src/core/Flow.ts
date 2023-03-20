6bd8c6eb40d1e42e6bf528403bae159696ff64df
Sérgio Avilla <sergiolucasavilladasilva@gmail.com>
2023-03-20 06:08:51 -0300

    add state and add get state by hash method

--- src/core/Flow.ts
+++ src/core/Flow.ts
@@ -4,9 +4,37 @@ export default class Flow {
     states: State[];
 
     constructor(
-        options: FlowOptions
+        readonly options: FlowOptions
     ) {
         this.states = [];
+    }
+
+    addStates(state: State): void {
+        this.states.push(state);
+    }
+
+    getStateByHash(hash: string): State {
+        if (!hash) {
+            return this.options.defaultState;
+        }
+
+        let state: State;
+        let localStates = [...this.states];
+        const hashParts = hash.split("|");
+
+        while (true) {
+            const filtredStates = localStates.filter(state => state.id === hashParts[0]);
+            
+            if (filtredStates.length < 1) {
+                return this.options.defaultState;
+            }
+
+            state = filtredStates[0];
+            localStates = state.branchs.map(branch => branch.to);
+            
+            hashParts.shift();
+            if (hashParts.length < 1) return state;
+        }
     }
 }
 
