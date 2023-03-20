60b59d2d45cfc21f0121f2a7a05baa32a790993c
Sérgio Avilla <sergiolucasavilladasilva@gmail.com>
2023-03-20 06:09:20 -0300

    handle message machine method init

--- src/core/Machine.ts
+++ src/core/Machine.ts
@@ -2,7 +2,6 @@ import ClientRepository from "../repositories/Client.repository";
 import Client from "./Client";
 import Flow from "./Flow";
 import Message from "./Message";
-import State from "./State";
 
 export default class Machine {
     constructor(
@@ -11,7 +10,17 @@ export default class Machine {
         private readonly sendMessage: (client: Client, message: Message) => Promise<void>
     ) { }
 
-    async handleMessage(phone: string, message: Message): Promise<void> {
-        // TODO: implement this method
+    async handleMessage(phone: string, message: Message, stateId?: string): Promise<void> {
+        let client: Client;
+
+        try {
+            client = await this.clientRepository.getClient(phone);
+        } catch (e) {
+            client = new Client(phone);
+            this.clientRepository.saveClient(client);
+        }
+
+        const hashState = client.hashState;
+
     }
 }
