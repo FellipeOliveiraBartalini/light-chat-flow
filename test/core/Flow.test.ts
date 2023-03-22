3850b18b18577976934bfb60269d126747d7e87e
Sérgio Avilla <sergiolucasavilladasilva@gmail.com>
2023-03-22 14:43:51 -0300

    adapt flow to new format

--- test/core/Flow.test.ts
+++ test/core/Flow.test.ts
@@ -2,32 +2,22 @@ import Flow from "../../src/core/Flow"
 import State from "../../src/core/State";
 import labelEqualsText from "../../src/utils/matchFunctions/labelEqualsText";
 
-const ID = "a998097777";
-
-const start = new State("start", {
-    text: "Olá meu povo!"
-}, {
-    text: "Desculpe não entendi vc!"
-}, labelEqualsText);
-
-const end = new State("end", {
-    text: "Foi bom te ver"
-}, {
-    text: "Tu é muito chato, não te entendo!"
-}, labelEqualsText);
-
-start
-    .createBranch("Oi", end);
-
 let flow: Flow;
 
 beforeEach(() => {
     flow = new Flow({
-        defaultState: start
+        defaultStateId: "start",
+        defaultMatchFunction: labelEqualsText
     });
 
-    flow.addStates(start);
-})
+    flow
+        .createState("start", "Olá meu povo!", "Desculpe, não entendi vc!", (newState) => {
+            newState
+                .branch("Oi")
+                .state("end", "Foi bom te ver", "Tu é muito chato, não te entendo!", labelEqualsText);
+            return;
+        });
+});
 
 test("Testando get state by client hash - start", () => {
     const state = flow.getStateByHash("start");
