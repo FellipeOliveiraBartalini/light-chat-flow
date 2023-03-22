4d64fd0f1aeaccb3ca47f6f06fa57f093c8a64d8
Sérgio Avilla <sergiolucasavilladasilva@gmail.com>
2023-03-22 14:43:07 -0300

    create createState method

--- src/core/Flow.ts
+++ src/core/Flow.ts
@@ -1,4 +1,6 @@
-import State from "./State";
+import State, { MatchFunction } from "./State";
+
+export type CreateStateCallback = (newState: State, thisFlow: Flow) => void;
 
 export default class Flow {
     states: State[];
@@ -15,29 +17,65 @@ export default class Flow {
 
     getStateByHash(hash: string): State {
         if (!hash) {
-            return this.options.defaultState;
+            return this.getDefaultState();
         }
 
         let state: State;
-        let localStates = [...this.states];
+        let localStates: (State | undefined)[] = [...this.states];
         const hashParts = hash.split("|");
 
         while (true) {
-            const filtredStates = localStates.filter(state => state.id === hashParts[0]);
+            const filtredStates = localStates.filter(state => state && state.id === hashParts[0]);
             
             if (filtredStates.length < 1) {
-                return this.options.defaultState;
+                return this.getDefaultState();
             }
 
+            if (!filtredStates[0]) throw "State not found";
+
             state = filtredStates[0];
-            localStates = state.branchs.map(branch => branch.to);
+            localStates = state.branchs
+                .filter(branch => branch.to)
+                .map(branch => branch.to);
             
             hashParts.shift();
             if (hashParts.length < 1) return state;
         }
     }
+
+    createState(id: string, message: string, catchMessage: string, createStateCallback?: CreateStateCallback, matchFunction?: MatchFunction): Flow {
+        const matchFunctionDefined = matchFunction ? matchFunction : this.options.defaultMatchFunction;
+        if (!matchFunctionDefined) throw "Missing matchFn";
+
+        const newState = new State(id, {
+            text: message
+        }, {
+            text: catchMessage
+        }, matchFunctionDefined);
+
+        if (createStateCallback) {
+            createStateCallback(newState, this);
+        }
+
+        this.addStates(newState);
+        return this;
+    }
+
+    getDefaultState(): State {
+        if (this.options.defaultState) return this.options.defaultState;
+        if (this.options.defaultStateId) return this.getStateById(this.options.defaultStateId);
+        throw "Default state not defined";
+    }
+
+    getStateById(id: string): State {
+        const filtredStates = this.states.filter(state => state.id === id);
+        if (filtredStates.length < 1) throw `State not found by id: ${id}`;
+        return filtredStates[0];
+    }
 }
 
 export interface FlowOptions {
-    defaultState: State
+    defaultState?: State,
+    defaultStateId?: string,
+    defaultMatchFunction?: MatchFunction
 }
