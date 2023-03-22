183c7ae83244177b900d6eb8db2d44c43f9dd7e6
Sérgio Avilla <sergiolucasavilladasilva@gmail.com>
2023-03-22 14:42:04 -0300

    to is optional, addTo method create, state method to add state

--- src/core/Branch.ts
+++ src/core/Branch.ts
@@ -1,13 +1,30 @@
 import Client from "./Client";
 import Message from "./Message";
-import State from "./State";
+import State, { MatchFunction } from "./State";
 
 export type BranchCallback = (message: Message, client: Client) => Promise<void>;
 
 export default class Branch {
+    to?: State;
+
     constructor(
         readonly label: string,
-        readonly to: State,
+        to?: State,
         readonly callback?: BranchCallback
-    ) { }
+    ) {
+        if (to) this.to = to;
+    }
+
+    addTo(to: State) {
+        this.to = to;
+    }
+
+    state(id: string, message: string, catchMessage: string, matchFunction: MatchFunction): void {
+        const newState = new State(id, {
+            text: message
+        }, {
+            text: catchMessage
+        }, matchFunction);
+        this.addTo(newState);
+    }
 }
