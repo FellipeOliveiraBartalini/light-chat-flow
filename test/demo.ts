dac4a974ba821be4c370bc0246845e4b125b63c7
Sérgio Avilla <sergiolucasavilladasilva@gmail.com>
2023-03-24 16:28:06 -0300

    improve demo

--- test/demo.ts
+++ test/demo.ts
@@ -18,17 +18,17 @@ const flow = new Flow({
     defaultMatchFunction: labelEqualsTextOrNumber
 });
 
-flow
-    .createState("start", "Olá meu povo!", "Desculpe, não entendi vc!", (newState) => {
-        newState
-            .branch("Oi")
-            .state("end", "Foi bom te ver", "Tu é muito chato, não te entendo!", labelEqualsTextOrNumber);
+flow.createState("start", "Olá, aqui é o plínio.", "Desculpa, não te entendi", (newState) => {
+    const educado = newState.branch("Olá").state("educado", "Que bom saber que você é um cliente educado", "Eu não te entendi meu bom amigo", labelEqualsTextOrNumber)
 
-        newState
-            .branch("Ola")
-            .state("endDois", "Você é um cara muito bacana, falou...", "Sai da minha frente", labelEqualsTextOrNumber);
-        return;
-    });
+    educado.branch("Sou mesmo!").state("soumemo", "Ish, que arrogância", "Eu não te entendi arrogante.", labelEqualsTextOrNumber)
+    educado.branch("Será!?").state("sera", "Aí vc me complica compadre", "Não te entendi compadre", labelEqualsTextOrNumber);
+
+    const chato = newState.branch("Hum").state("chato", "Que difícil hein, vc é um cara chato, que pena", "Fala de novo", labelEqualsTextOrNumber);
+
+    chato.branch("Sou mesmo!").state("souchatomemo", "Ish, que arrogância", "Eu não te entendi arrogante.", labelEqualsTextOrNumber)
+    chato.branch("Não sou não!").state("naosouchato", "Vamos descobrir", "Eu não te entendi inoscente.", labelEqualsTextOrNumber);
+});
 
 const machine = new Machine(flow, clientRepository, sendMessageGateway);
 
